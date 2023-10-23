77e8de56fc7da5a19897de09ed1b750b6eba34a1
Tomi <tomjames156@gmail.com>
2023-10-23 22:39:58 +0100

    added languages and technologies used as many to many fields

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -2,24 +2,29 @@ from django.db import models
 
 # Create your models here.
 
-class Project(models.Model):
-    project_name = models.CharField(max_length=200, null=False, blank=False)
-    about_project = models.CharField(max_length=5000, null=True, blank=True) # to-do change the blankness 
+class LanguageUsed(models.Model):
+    language_name = models.CharField(max_length=300, blank=False, null=False)
 
     def __str__(self):
-        return self.project_name
-    
+        return self.language_name
 
 class TechnologyUsed(models.Model):
     technology_name = models.CharField(max_length=100, blank=False, null=False)
 
     def __str__(self):
         return self.technology_name
-    
 
-
-class LanguageUsed(models.Model):
-    language_name = models.CharField(max_length=300, blank=False, null=False)
+class Project(models.Model):
+    project_name = models.CharField(max_length=200, null=False, blank=False)
+    about_project = models.CharField(max_length=5000, null=True, blank=True) # to-do change the blankness
+    technology_used = models.ManyToManyField(TechnologyUsed)
+    language_used = models.ManyToManyField(LanguageUsed)
+    levels = [
+        (1, 'complex'),
+        (2, 'proud of'),
+        (3, 'easy')
+    ]
+    importance = models.CharField(choices=levels, max_length=1)
 
     def __str__(self):
-        return self.language_name
+        return self.project_name
