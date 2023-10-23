18f0f9d5b9ecb99f15fbf1ea9f2341c0a7b24c06
Tomi <tomjames156@gmail.com>
2023-10-23 03:18:01 +0100

    added languages used model

--- backend/projects/admin.py
+++ backend/projects/admin.py
@@ -3,3 +3,5 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Project)
+admin.site.register(TechnologyUsed)
+admin.site.register(LanguageUsed)
--- backend/projects/models.py
+++ backend/projects/models.py
@@ -4,6 +4,22 @@ from django.db import models
 
 class Project(models.Model):
     project_name = models.CharField(max_length=200, null=False, blank=False)
+    about_project = models.CharField(max_length=5000, null=True, blank=True) # to-do change the blankness 
 
     def __str__(self):
         return self.project_name
+    
+
+class TechnologyUsed(models.Model):
+    technology_name = models.CharField(max_length=100, blank=False, null=False)
+
+    def __str__(self):
+        return self.technology_name
+    
+
+
+class LanguageUsed(models.Model):
+    language_name = models.CharField(max_length=300, blank=False, null=False)
+
+    def __str__(self):
+        return self.language_name
