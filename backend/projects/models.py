2895028659db4ee3406667c5168ed298d44c44f6
Tomi <tomjames156@gmail.com>
2023-10-23 23:29:24 +0100

    fixed anguage used bug

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class LanguageUsed(models.Model):
-    language_name = models.CharField(max_length=300, blank=False, null=False)
+    language_name = models.CharField(max_length=300, null=True, blank=True)
 
     def __str__(self):
         return self.language_name
@@ -11,15 +11,19 @@ class LanguageUsed(models.Model):
     class Meta:
         verbose_name_plural = 'Languages Used'
 
+
+
 class TechnologyUsed(models.Model):
     technology_name = models.CharField(max_length=100, blank=False, null=False)
-    language_used = models.ForeignKey(LanguageUsed, on_delete=models.CASCADE, null=False)
+    language_used = models.ForeignKey(LanguageUsed, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.technology_name
     
     class Meta:
         verbose_name_plural = 'Technologies Used'
+
+
 
 class Project(models.Model):
     project_name = models.CharField(max_length=200, null=False, blank=False)
