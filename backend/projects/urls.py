c3ba1f11df637e7677edbcb3adabc1c01ef340f2
Tomi <tomjames156@gmail.com>
2023-10-24 23:44:36 +0100

    added get projects by the language used to built them

--- backend/projects/urls.py
+++ backend/projects/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('welcome/', welcome, name='welcome'),
     path('projects/', get_projects, name='all_projects'),
     path('technologies/', get_technologies, name='my_tech_stack'),
-    path('technology_used/<str:technology_used>', get_project_by_technology, name='project_built_with')
+    path('technology_used/<str:technology_used>', get_project_by_technology, name='project_built_with_technology'),
+    path('language_used/<str:language_name>', get_project_by_language, name='project_built_with_language')
 ]
--- backend/projects/views.py
+++ backend/projects/views.py
@@ -33,3 +33,29 @@ def get_project_by_technology(request, technology_used):
         return Response(serializer.data)
     except (TechnologyUsed.DoesNotExist, ValueError):
         return Response(f"I haven't built anything with {technology_used}")
+    
+
+@api_view(['GET'])
+def get_project_by_language(request, language_name):
+    try:
+        language = LanguageUsed.objects.get(language_name=language_name)
+        built_with_language = TechnologyUsed.objects.filter(language_used=language)
+        print(built_with_language)
+        # if list(built_with_language) != []:
+        #     tech_stack
+        # technology_used=TechnologyUsed.objects.get()
+        projects_language = Project.objects.filter(language_used=language)
+        serializer = ProjectSerializer(projects_language, many=True)
+        return Response(serializer.data)
+    except (LanguageUsed.DoesNotExist, ValueError):
+        return Response(f"I haven't built anything with {language_name}")
+    
+
+@api_view(['GET'])
+def get_technologies_language_built(request, language_name):
+    try:
+        technolgies = TechnologyUsed.objects.filter(language_name=language_name)
+        serializer = TechnologySerializer(technolgies, many=True)
+        return Response(serializer.data)
+    except (TechnologyUsed.DoesNotExist, ValueError):
+        return Response(f'No {language_name} frameworks in my tech stack')
