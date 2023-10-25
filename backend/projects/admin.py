cff1b6d6eae89a08d8946c56af5f61c9326cabdb
Tomi <tomjames156@gmail.com>
2023-10-25 23:08:45 +0100

    updated project admin

--- backend/projects/admin.py
+++ backend/projects/admin.py
@@ -3,9 +3,12 @@ from .models import *
 
 class ProjectAdmin(admin.ModelAdmin):
     fieldsets = [
-        ("About the Project", {"fields": ['project_name', 'about_project']})
+        ("About the Project", {"fields": ['project_name', 'project_description', 'project_image']}),
+        ("Built With", {"fields": ['technology_used', 'language_used']}),
+        ("Importance / Complexity", {"fields": ["importance"]})
     ]
 
-admin.site.register(Project)
+
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(TechnologyUsed)
 admin.site.register(LanguageUsed)
