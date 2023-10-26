0819a5beea136cb48e11950a0f56bfaf4f562e62
Tomi <tomjames156@gmail.com>
2023-10-26 12:04:38 +0100

    added live site inline

--- backend/projects/admin.py
+++ backend/projects/admin.py
@@ -10,13 +10,19 @@ class RepositoryInline(admin.StackedInline):
     model = Repository
     extra = 2
 
+
+class LiveSiteInline(admin.StackedInline):
+    model = LiveSite
+    extra = 1
+
+
 class ProjectAdmin(admin.ModelAdmin):
     fieldsets = [
         ("Overview", {"fields": ['project_name', 'project_description', 'project_image', "complexity"]}),
         ("Built With", {"fields": ['technologies_used', 'languages_used']}),
         ("View Project", {"fields": ['hosted', 'hostable']})
     ]
-    inlines = [ScreenShotInline, RepositoryInline]
+    inlines = [ScreenShotInline, RepositoryInline, LiveSiteInline]
 
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(TechnologyUsed)
