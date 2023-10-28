5b7a6a6d7650cd2a0a8d1c5fefd0e7e2cca2d767
Tomi <tomjames156@gmail.com>
2023-10-28 09:58:47 +0100

    updated user info view

--- backend/about_me/views.py
+++ backend/about_me/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 from .serializers import *
 from .models import *
 
 from django.contrib.auth.models import User
 
+@api_view(['GET'])
 def get_user_info(request, username):
     user = User.objects.get(username=username)
     user_info = UserPortfolio.objects.get(user=user)
-    print(user_info.bio)
-    return HttpResponse(user_info.bio)
+    serializer = UserPortfolioSerializer(user_info)
+    return Response(serializer.data)
