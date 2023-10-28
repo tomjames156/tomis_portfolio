82921133c946b359f4de28fadc27e835695cd179
Tomi <tomjames156@gmail.com>
2023-10-28 22:44:47 +0100

    updated admin sitte for about_me app

--- backend/about_me/admin.py
+++ backend/about_me/admin.py
@@ -1,7 +1,36 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(BulletPoint)
-admin.site.register(UserPortfolio)
-admin.site.register(SocialLink)
-admin.site.register(Experience)
+
+class SocialMediaInline(admin.StackedInline):
+    model = SocialLink
+    extra = 3
+    verbose_name_plural = 'Social Media'
+    verbose_name = 'Social Media Link'
+
+
+class UserPortfolioAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Basic Info", {"fields": ['user', 'email', 'bio']})
+    ]
+    inlines = [SocialMediaInline]
+
+admin.site.register(UserPortfolio, UserPortfolioAdmin)
+
+
+class BulletPointInline(admin.StackedInline):
+    model = BulletPoint
+    extra = 3 
+
+
+class ExperienceAdmin(admin.ModelAdmin):
+    model = Experience
+    verbose_name_plural = 'Work Experience'
+
+    fieldsets = [
+        ("Role & Company", {'fields': ["user", "role", "company", "timeline"]})
+    ]
+    
+    inlines = [BulletPointInline]
+    
+admin.site.register(Experience, ExperienceAdmin)
