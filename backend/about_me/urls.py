5051aea9aacf0d69fcde79c3d183ac28d6ac3f96
Tomi <tomjames156@gmail.com>
2023-10-29 08:25:02 +0100

    added documentation for about me

--- backend/about_me/urls.py
+++ backend/about_me/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('<str:username>',  get_user_info, name="get user info"),
+    path('', initial, name='api_info'),
+    path('<str:username>/',  get_user_info, name="get user info"),
     path('<str:username>/work_experience', get_experience, name="get user work experience")
 ]
--- backend/about_me/views.py
+++ backend/about_me/views.py
@@ -8,8 +8,31 @@ from .models import *
 
 from django.contrib.auth.models import User
 
+
+@api_view(['GET'])
+def initial(request):
+    "Describes the api endpoints, access methods and return values"
+    routes = [
+        {
+            'Endpoint': 'about/username/',
+            'method': 'GET',
+            'body': None,
+            'description': "Returns an existing user's basic information like the email, bio, and social media accounts."
+        },
+        {
+            'Endpoint': 'about/username/work_experience/',
+            'method': 'GET',
+            'body': None,
+            'description': "Returns a list an existing user's work experience"
+        }
+    ]
+
+    return Response(routes)
+
+
 @api_view(['GET'])
 def get_user_info(request, username):
+    "Gets a user's info based on username provided"
     user = User.objects.get(username=username)
     user_info = UserPortfolio.objects.get(user=user)
     serializer = UserPortfolioSerializer(user_info)
@@ -18,6 +41,7 @@ def get_user_info(request, username):
 
 @api_view(['GET'])
 def get_experience(request, username):
+    "Gets a user's work experience based on username provided"
     user = User.objects.get(username=username)
     users_portfolio = UserPortfolio.objects.get(user=user)
     work_experience = users_portfolio.work_experience.all()
